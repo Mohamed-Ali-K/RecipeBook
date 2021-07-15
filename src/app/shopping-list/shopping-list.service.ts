71761d8c372963fee017a7231b1e55951053dbe6
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-07-15 23:39:46 +0100

    reforming the shopping list and shopping edit and using service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShoppingListService {
-
-  constructor() { }
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+  constructor() {}
+  getIngredient() {
+    return [...this.ingredients];
+  }
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
