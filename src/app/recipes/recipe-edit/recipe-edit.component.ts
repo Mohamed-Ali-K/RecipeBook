8d5f5d1a1bfd42fc7f05cd87858bb3413588c0c7
Mohamed-A-K <kenis.mohamed.ali@gmail.com>
2021-07-23 13:46:28 +0100

    Syncing HTML with the Form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -24,6 +24,10 @@ export class RecipeEditComponent implements OnInit {
       this.initForm();
     });
   }
+  onSubmit() {
+    console.log(this.recipeForm);
+
+  }
 
   private initForm() {
     let recipeName = '';
