e60678e00b382353f69f905a5d8fe512986673b1
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-07-26 16:28:57 +0100

    Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
-  constructor(private http: HttpClient) { }
+  storeRecipe() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://recipebook-91-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
