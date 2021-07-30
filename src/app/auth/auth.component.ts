a239e0f40b2a02055bece492656579b06970470b
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-07-30 11:47:28 +0100

    Improving Error Handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -34,10 +34,9 @@ export class AuthComponent implements OnInit {
         (response) => {
           console.log(response);
           this.isLoading= false;
-        },
-        (error) => {
-          console.log(error);
-          this.error = 'An error occurred!';
+        }, errorMessage => {
+          console.log(errorMessage);
+          this.error = errorMessage;
           this.isLoading = false;
         }
       );
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/Operators';
 import { environment } from 'src/environments/environment';
 
 interface AuthResponseData {
@@ -17,13 +19,28 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   SignUp(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`,
-      {
-        email,
-        password,
-        returnSecureToken: true,
-      }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`,
+        {
+          email,
+          password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorRes: HttpErrorResponse) => {
+          let errorMessage = 'An unknown error occurred!';
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage =
+                'The email address is already in use by another account.';
+          }
+          return throwError(errorMessage);
+        })
+      );
   }
 }
