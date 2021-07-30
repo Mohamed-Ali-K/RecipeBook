8df10ab7cfedac66c11d547d8db15c6b13551cce
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-07-30 10:53:15 +0100

    Adding a Loading Spinner & Error Handling Logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { AuthComponent } from './auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { AuthComponent } from './auth/auth.component';
     DropdownDirective,
     RecipeEditComponent,
     RecipeStartComponent,
-    AuthComponent
+    AuthComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -10,6 +10,8 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent implements OnInit {
   isLoginMode = true;
+  isLoading = false;
+  error! :string;
   signUpSub = Subscription;
   constructor(private authService: AuthService) {}
 
@@ -23,15 +25,20 @@ export class AuthComponent implements OnInit {
     }
     const email = form.value.email;
     const password = form.value.password;
+
+    this.isLoading = true;
     if (this.isLoginMode) {
       //...
     } else {
       this.authService.SignUp(email, password).subscribe(
         (response) => {
           console.log(response);
+          this.isLoading= false;
         },
         (error) => {
           console.log(error);
+          this.error = 'An error occurred!';
+          this.isLoading = false;
         }
       );
     }
