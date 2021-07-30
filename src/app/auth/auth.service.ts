d0cfa548c9f825acdfa1de0e030daafdf998f89d
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-07-30 12:25:26 +0100

    improving the error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -29,23 +29,10 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(
-        catchError((errorRes: HttpErrorResponse) => {
-          let errorMessage = 'An unknown error occurred!';
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMessage);
-          }
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage =
-                'The email address is already in use by another account.';
-          }
-          return throwError(errorMessage);
-        })
-      );
+      .pipe(catchError(this.handelErro));
   }
   Login(email: string, password: string) {
-   return this.http
+    return this.http
       .post<AuthResponseData>(
         `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
         {
@@ -54,19 +41,39 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(
-        catchError((errorRes: HttpErrorResponse) => {
-          let errorMessage = 'An unknown error occurred!';
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMessage);
-          }
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_NOT_FOUND':
-              errorMessage =
-                'There is no user record corresponding to this identifier. The user may have been deleted.';
-          }
-          return throwError(errorMessage);
-        })
-      );
+      .pipe(catchError(this.handelErro));
+  }
+
+  private handelErro(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_NOT_FOUND':
+        errorMessage =
+          'There is no user record corresponding to this identifier. The user may have been deleted.';
+        break;
+      case 'EMAIL_EXISTS':
+        errorMessage =
+          'The email address is already in use by another account.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage =
+          ' The password is invalid or the user does not have a password.';
+        break;
+      case 'USER_DISABLED':
+        errorMessage =
+          ' The user account has been disabled by an administrator.';
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = ' Password sign-in is disabled for this project.';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage =
+          'We have blocked all requests from this device due to unusual activity. Try again later...';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
