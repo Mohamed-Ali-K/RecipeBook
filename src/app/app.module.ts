691e6ed1c0b299f0346e963067bb06325241b46d
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-08-02 20:42:03 +0100

    adding the Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { RecipesModule } from './recipes/recipes.module';
+import { CoreModule } from './core/core.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
-
-import { AuthInterceptor } from './auth/auth.interceptor';
 
 import { AuthComponent } from './auth/auth.component';
 import { AppComponent } from './app.component';
@@ -24,9 +23,7 @@ import { HeaderComponent } from './header/header.component';
     RecipesModule,
     ShoppingListModule,
     SharedModule,
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    CoreModule,
   ],
   bootstrap: [AppComponent],
 })
